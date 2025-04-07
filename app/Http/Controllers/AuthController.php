5c11914e94bd493cff7841c45ca756a533b5a11c
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-04-08 00:04:12 +0100

    implement register and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails())
+            return $validator->errors();
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json($user);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails())
+            return $validator->errors();
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ]);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'user' => $user]);
+    }
+}
