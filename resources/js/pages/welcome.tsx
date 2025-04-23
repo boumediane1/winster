90cd74b3027f3c5560b4198a292d0ffc065403b6
Otmane Boumediane <boumediane.othmane@gmail.com>
2025-04-23 19:01:55 +0100

    install prettier

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,5 +1,5 @@
-const Welcome = ({user}: { user: { name: string } }) => {
-    return <h1>Welcome {user.name}</h1>
-}
+const Welcome = ({ user }: { user: { name: string } }) => {
+    return <h1>Welcome {user.name}</h1>;
+};
 
 export default Welcome;
